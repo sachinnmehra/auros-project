738f2ed3398a3b29989127ef617bdb4672f137ad
SACHIN MEHRA <sachinmehra1398@gmail.com>
2021-04-19 21:29:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          <Route path="/" component={HomePage} />
 
           <Route path="/shop" component={ShopPage} />
           <Route
